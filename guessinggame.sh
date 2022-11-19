eb166d3d6ca2a7ebb3e722a03529feeb63c518dd
Alexis La Porte <aklaporte@gmail.com>
2022-11-18 20:31:48 -0500

    Add game logic and loop

---
+++ guessinggame.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function game {
+	local guess
+	local answer=$(ls | wc -l)
+
+	echo "Guess how many files are in the current directory"
+	read response
+
+	while [[ $response -ne $answer ]]
+	do
+		if [[ $response -lt $answer ]]
+		then
+			hint="too low"
+		elif [[ $response -gt $answer ]]
+		then
+			hint="too high"
+		fi
+		echo "$hint"
+		game
+	done
+}
+
+game
+echo "correct"
