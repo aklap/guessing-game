d2a6ee39faec1b0d5641d209a89286fd1938009d
Alexis La Porte <aklaporte@gmail.com>
2022-11-18 21:05:04 -0500

    Fix spacing

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 function game {
-	local guess
-	local answer=$(ls | wc -l)
+local guess
+local answer=$(ls | wc -l)
 
-	echo "Guess how many files are in the current directory"
-	read response
+echo "Guess how many files are in the current directory"
+read response
 
-	while [[ $response -ne $answer ]]
-	do
-		if [[ $response -lt $answer ]]
-		then
-			hint="too low"
-		elif [[ $response -gt $answer ]]
-		then
-			hint="too high"
-		fi
-		echo "$hint"
-		game
-	done
+while [[ $response -ne $answer ]]
+do
+  if [[ $response -lt $answer ]]
+  then
+    hint="too low"
+  elif [[ $response -gt $answer ]]
+    then
+      hint="too high"
+    fi
+    echo "$hint"
+    game
+  done
 }
 
 game
